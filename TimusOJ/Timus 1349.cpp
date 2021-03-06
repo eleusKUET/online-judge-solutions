9329929d6ccaf3ef165b8f769f414fbdfb9626c6
eleusKUET <60877162+eleusKUET@users.noreply.github.com>
2021-03-06 12:41:14 +0600

    Create Timus 1349.cpp

---
+++ TimusOJ/Timus 1349.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+signed main()
+{
+    int n; cin >> n;
+    if (n == 1) {
+        puts("1 2 3");
+    }
+    else if (n == 2) {
+        puts("3 4 5");
+    }
+    else {
+        puts("-1");
+    }
+}
