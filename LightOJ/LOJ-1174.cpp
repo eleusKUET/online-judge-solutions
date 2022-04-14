2747eda2e943cc93a05394fa7333454e52e38fee
eleusKUET <60877162+eleusKUET@users.noreply.github.com>
2022-04-14 22:08:13 +0600

    Create LOJ-1174.cpp

---
+++ LightOJ/LOJ-1174.cpp
@@ -0,0 +1,117 @@
+#include "bits/stdc++.h"
+using namespace std;
+//#define ONLINE_JUDGE
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+#define space " "
+#define nline "\n"
+
+const int mod = 1e9 + 7;
+
+const int N = 100;
+
+vi g[N];
+int n, r, s, d;
+int ans;
+vi vis(N), from(N), to(N);
+
+void bfs(int u, vi &dist)
+{
+    for (int i = 0; i < n; i++) {
+        vis[i] = 0;
+    }
+
+    queue<int> q;
+    q.push(u);
+    dist[u] = 0;
+    vis[u]++;
+
+    while (q.size()) {
+        int u = q.front();
+        q.pop();
+
+        for (auto v : g[u]) {
+            if (vis[v] == 0) {
+                vis[v]++;
+                q.push(v);
+                dist[v] = dist[u] + 1;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    cin >> n >> r;
+    for (int i = 0; i < n; i++) {
+        g[i].clear();
+        vis[i] = 0;
+        from[i] = 0;
+        to[i] = 0;
+    }
+    for (int i = 0; i < r; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+
+    cin >> s >> d;
+    bfs(s, from);
+    bfs(d, to);
+
+    ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, from[i] + to[i]);
+    }
+    cout << ans << nline;
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    int cs = 1;
+    while (t--) {
+        cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
