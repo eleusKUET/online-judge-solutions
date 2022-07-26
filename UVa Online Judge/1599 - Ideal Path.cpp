af53003db18c110bab758d0b65e94f09193bb7f7
eleusKUET <60877162+eleusKUET@users.noreply.github.com>
2022-07-26 15:46:25 +0600

    Create 1599 - Ideal Path.cpp

---
+++ UVa Online Judge/1599 - Ideal Path.cpp
@@ -0,0 +1,196 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+//#define ONLINE_JUDGE
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T_vector>
+void output_vector(T_vector a, int lo = -1, int hi = -1, bool line = false)
+{
+    if (lo == -1) lo = 0;
+    if (hi == -1) hi = a.size();
+    for (int i = lo; i < hi; i++) {
+        cout << a[i] << (line ? "\n" : " ");
+    }
+    cout << "\n";
+}
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+
+const int mod = 1e9 + 7;
+
+const int N = 1e5;
+
+int n, m;
+vpi g[N + 5];
+vi d1(N + 5);
+vi d2(N + 5);
+
+void solve()
+{
+    while (cin >> n >> m) {
+        map<pii,int> rep;
+        for (int i = 0; i <= n; i++) {
+            g[i].clear();
+            d1[i] = 0;
+            d2[i] = 0;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            if (u == v) continue;
+            auto &it = rep[{u, v}];
+            if (it) it = min(w, it);
+            else it = w;
+        }
+        
+        for (auto [node, w] : rep) {
+            auto [u, v] = node;
+            g[u].pb({w, v});
+            g[v].pb({w, u});
+        }
+        
+        queue<int> q;
+        q.push(n);
+        
+        d1[n] = 1;
+        
+        while (q.size()) {
+            int u = q.front(); 
+            q.pop();
+            
+            for (auto [w, v] : g[u]) {
+                if (d1[v]) continue;
+                d1[v] = d1[u] + 1;
+                q.push(v);
+            }
+        }
+        q.push(1);
+        
+        d2[1] = 1;
+        
+        while (q.size()) {
+            int u = q.front(); 
+            q.pop();
+            
+            for (auto [w, v] : g[u]) {
+                if (d2[v]) continue;
+                d2[v] = d2[u] + 1;
+                q.push(v);
+            }
+        }
+        vi mark(n + 4);
+        
+        for (int i = 1; i <= n; i++) {
+            if (d1[i] + d2[i] - 1 != d1[1]) mark[i] = 1;
+        }
+        vi ans(n + 5, 2e9);
+        
+        queue<pii> pq;
+        
+        pq.push({2e9, 1});
+        
+        while (pq.size()) {
+            auto [col, u] = pq.front();
+            pq.pop();
+            if (mark[u] || col > ans[d2[u]]) continue;
+            // debug(pq.front());
+            mark[u] = 1;
+            for (auto [w, v] : g[u]) {
+                if (mark[v]) continue;
+                if (d1[u] == d1[v] + 1 && ans[d2[v]] >= w) {
+                    ans[d2[v]] = w;
+                    pq.push({w, v});
+                }
+            }
+        }
+        
+        cout << d2[n] - 1 << "\n";
+        for (int i = 2; i <= d2[n]; i++) {
+            cout << ans[i];
+            if (i + 1 <= d2[n]) cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
