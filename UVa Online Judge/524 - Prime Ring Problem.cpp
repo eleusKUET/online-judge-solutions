3dc741cc4aa2147d3e68a902c5af3d34d182024e
eleusKUET <60877162+eleusKUET@users.noreply.github.com>
2022-08-01 17:10:34 +0600

    Create 524 - Prime Ring Problem.cpp

---
+++ UVa Online Judge/524 - Prime Ring Problem.cpp
@@ -0,0 +1,142 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+//#define ONLINE_JUDGE
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T_vector>
+void output_vector(T_vector a, int lo = -1, int hi = -1, char sep = ' ')
+{
+    if (lo == -1) lo = 0;
+    if (hi == -1) hi = a.size();
+    for (int i = lo; i < hi; i++) {
+        cout << a[i];
+        if (i + 1 < hi) cout << sep;
+    }
+    cout << "\n";
+}
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+
+const int mod = 1e9 + 7;
+
+const int N = 2e5;
+
+int n;
+vi path;
+bool vis[20];
+bool prime[50];
+
+void f()
+{
+    if (path.size() == n) {
+        output_vector<vi> (path);
+        return;
+    }
+    
+    for (int i = 2; i <= n; i++) {
+        if (vis[i]) continue;
+        if (prime[path.back() + i] == false) continue;
+        if (path.size() == n - 1 && prime[1 + i] == false) break;
+        vis[i] = true;
+        path.pb(i);
+        f();
+        path.pop_back();
+        vis[i] = false;
+    }
+}
+
+
+void solve()
+{
+    prime[2] = prime[3] = prime[5] = prime[7] = prime[11] = prime[13] = prime[17] = prime[19] = prime[23] = prime[29] = prime[31] = true;
+    int cs = 1;
+    while (cin >> n) {
+        memset(vis, 0, sizeof vis);
+        path.clear();
+        path.pb(1);
+        if (cs > 1) cout << "\n";
+        cout << "Case " << cs++ << ":" << "\n";
+        f();
+    }
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
