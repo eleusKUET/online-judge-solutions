c4db8e499800bd1441367577cc42b5579f9c91b6
eleusKUET <60877162+eleusKUET@users.noreply.github.com>
2022-09-01 17:52:45 +0600

    Create 1625 - Color Length.cpp

---
+++ UVa Online Judge/1625 - Color Length.cpp
@@ -0,0 +1,140 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifdef __LOCAL__
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 5005;
+
+int dp[N][N];
+int n, m;
+int cntA[N][30];
+int cntB[N][30];
+
+int isLast(int i, int j, char ch)
+{
+    ch -= 'A';
+    if (cntA[i][ch] + cntB[j][ch] == cntA[n][ch] + cntB[m][ch]) return i + j;
+    return 0;
+}
+
+int isFirst(int i, int j, char ch)
+{
+    ch -= 'A';
+    if (cntA[i][ch] + cntB[j][ch] == 1) return i + j;
+    return 0;
+}
+
+string a, b;
+
+void solve()
+{
+    cin >> a >> b;
+    n = a.size();
+    m = b.size();
+    
+    a = "#" + a;
+    b = "#" + b;
+    
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            dp[i][j] = 1<<30;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < 26; j++) {
+            cntA[i][j] = cntA[i - 1][j];
+        }
+        cntA[i][a[i] - 'A']++;
+    }
+    for (int i = 1; i <= m; i++) {
+        for (int j = 0; j < 26; j++) {
+            cntB[i][j] = cntB[i - 1][j];
+        }
+        cntB[i][b[i] - 'A']++;
+    }
+    
+    dp[0][0] = 0;
+    
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (i == 0 && j == 0) continue;
+            if (i == 0) {
+                dp[i][j] = dp[i][j - 1] + isLast(i, j, b[j]) - isFirst(i, j, b[j]);
+            }
+            else if (j == 0) {
+                dp[i][j] = dp[i - 1][j] + isLast(i, j, a[i]) - isFirst(i, j, a[i]);
+            }
+            else {
+                dp[i][j] = dp[i][j - 1] + isLast(i, j, b[j]) - isFirst(i, j, b[j]);
+                dp[i][j] = min(dp[i][j], dp[i - 1][j] + isLast(i, j, a[i]) - isFirst(i, j, a[i]));
+            }
+        }
+    }
+    cout << dp[n][m] << "\n";
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
