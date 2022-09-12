95d49b07c02fe8c08ec421af6131230e16fca34c
eleusKUET <60877162+eleusKUET@users.noreply.github.com>
2022-09-12 12:32:02 +0600

    Linear Diophantine equation

---
+++ UVa Online Judge/12775 - Gift Dilemma.cpp
@@ -0,0 +1,167 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifdef __LOCAL__
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 1e5;
+
+#define int long long 
+
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+void shift_solution(int & x, int & y, int a, int b, int cnt) {
+    x += cnt * b;
+    y -= cnt * a;
+}
+
+int find_all_solutions(int a, int b, int c, int minx, int maxx, int miny, int maxy) {
+    int x, y, g;
+    if (!find_any_solution(a, b, c, x, y, g))
+        return 0;
+    a /= g;
+    b /= g;
+
+    int sign_a = a > 0 ? +1 : -1;
+    int sign_b = b > 0 ? +1 : -1;
+
+    shift_solution(x, y, a, b, (minx - x) / b);
+    if (x < minx)
+        shift_solution(x, y, a, b, sign_b);
+    if (x > maxx)
+        return 0;
+    int lx1 = x;
+
+    shift_solution(x, y, a, b, (maxx - x) / b);
+    if (x > maxx)
+        shift_solution(x, y, a, b, -sign_b);
+    int rx1 = x;
+
+    shift_solution(x, y, a, b, -(miny - y) / a);
+    if (y < miny)
+        shift_solution(x, y, a, b, -sign_a);
+    if (y > maxy)
+        return 0;
+    int lx2 = x;
+
+    shift_solution(x, y, a, b, -(maxy - y) / a);
+    if (y > maxy)
+        shift_solution(x, y, a, b, sign_a);
+    int rx2 = x;
+
+    if (lx2 > rx2)
+        swap(lx2, rx2);
+    int lx = max(lx1, lx2);
+    int rx = min(rx1, rx2);
+
+    if (lx > rx)
+        return 0;
+    return (rx - lx) / abs(b) + 1;
+}
+
+
+
+void solve()
+{
+    ll a, b, c, p;
+    cin >> a >>  b >> c >> p;
+    
+    int k = 0;
+    ll ans = 0;
+    while (true) {
+        int q = p - k * c;
+        if (q < 0) break;   
+        ans += find_all_solutions(a, b, q, 0, p, 0, p);
+        k++;
+    }
+    
+    cout << ans << "\n";
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    int cs = 1;
+    while (t--) {
+        cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
