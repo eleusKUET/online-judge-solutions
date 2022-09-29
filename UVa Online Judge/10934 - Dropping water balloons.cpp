d0a3d5918149dca82fbc22387643584782ca0594
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2022-09-29 13:02:30 +0600

    Create 10934 - Dropping water balloons.cpp

---
+++ UVa Online Judge/10934 - Dropping water balloons.cpp
@@ -0,0 +1,104 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifdef __LOCAL__
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 1e5;
+
+unsigned ll n, k;
+
+unsigned ll dp[105][64];
+
+void solve()
+{
+    cin >> k >> n;
+    if (k == 0) return;
+    memset(dp, 0, sizeof dp);
+    
+    for (int i = 1; i <= 100; i++) {
+        for (int j = 1; j <= 63; j++) {
+            if (j == 1) dp[i][j] = 1;
+            else dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1] + 1;
+        }
+    }
+    
+    int i;
+    for (i = 1; i <= 63; i++) {
+        if (dp[k][i] >= n) break;
+        //debug(dp[k][i], k, i);
+    }
+    
+    if (i > 63) {
+        cout << "More than 63 trials needed.\n";
+    }
+    else {
+        cout << i << "\n";
+    }
+    solve();
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
