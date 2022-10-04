9c59a9da50038eb0252ba1baee7b84f3a05a55b0
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2022-10-04 17:42:03 +0600

    Create 11401 - Triangle Counting.cpp

---
+++ UVa Online Judge/11401 - Triangle Counting.cpp
@@ -0,0 +1,103 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifdef __LOCAL__
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+#define int long long 
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 1e6 + 7;
+
+ll a[N];
+ll b[N];
+
+void preprocess()
+{
+    ll tot = 0;
+    ll sum = 0;
+    for (ll i = 3; i < N; i++) {
+        tot += (i - 1) / 2;
+        a[i] = tot;
+        sum += ((i - 1) / 2) * (i - 1);
+        b[i] = sum;
+    }
+}
+
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n < 3) return;
+
+    ll ans = n * (n - 1) * (n - 2) / 6;
+    ans -= n * a[n] - b[n];
+
+    cout << ans << "\n";
+
+    solve();
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    preprocess();
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
