9aa1b4d013998d61954426738ca0cc8d3b166468
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2022-10-27 17:00:24 +0600

    Idea : Euler's planar graph formula: v - e + f = 2

---
+++ UVa Online Judge/10178 - Count the Faces.cpp
@@ -0,0 +1,123 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template <class K,class V> using omap = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 1e5;
+
+
+void solve()
+{
+    int n, m;
+    while (cin >> n >> m) {
+        map<int,int> id;
+        int giveid = 0;
+
+        auto getId = [&](char a) {
+            return id[a] ? id[a] : id[a] = ++giveid;
+        };
+        int nodes, edges;
+        vi vis(500), g[500];
+
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            char u_, v_;
+            cin >> u_ >> v_;
+            u = getId(u_);
+            v = getId(v_);
+            g[u].pb(v);
+            g[v].pb(u);
+        }
+
+        function<void(int)> dfs = [&](int u) {
+            if (vis[u]) return;
+            vis[u]++;
+            nodes++;
+            edges += g[u].size();
+            for (auto v : g[u]) {
+                dfs(v);
+            }
+        };
+
+        int ans = 0;
+        int tot = 0;
+        for (auto [_, u] : id) {
+            if (vis[u]) continue;
+            nodes = 0;
+            edges = 0;
+            dfs(u);
+            edges /= 2;
+            tot += nodes;
+            ans += 1 + edges - nodes;
+        }
+        ans++;
+        cout<<ans << "\n";
+    }
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
