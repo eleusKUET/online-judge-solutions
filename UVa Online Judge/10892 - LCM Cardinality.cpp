6d3721a179152a1b728b0019783f013fc447015d
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2022-10-27 17:54:36 +0600

    Create 10892 - LCM Cardinality.cpp

---
+++ UVa Online Judge/10892 - LCM Cardinality.cpp
@@ -0,0 +1,102 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template <class K,class V> using omap = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long 
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 1e5;
+
+
+void solve()
+{
+    int n;
+    while (cin >> n && n) {
+        vi g;
+        for (int i = 1; i * i <= n; i++) {
+            if (n%i == 0) {
+                g.pb(i);
+                if (i * i != n) g.pb(n / i);
+            }
+        }
+        int tot = 0;
+        for (auto it : g) {
+            int cnt = 0;
+            int h = n / it;
+            for (int i = 1; i * i <= h; i++) {
+                if (h % i == 0) {
+                    if (__gcd(i, h / i) == 1) cnt++;
+                }
+            }
+
+            tot += cnt;
+        }
+
+        cout << n << " " << tot << "\n";
+    }
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
