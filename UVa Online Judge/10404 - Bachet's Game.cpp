0db6cc4debc6fea525bdc0aab9c639c32aa19b38
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2022-12-06 23:02:32 +0600

    Create 10404 - Bachet's Game.cpp

---
+++ UVa Online Judge/10404 - Bachet's Game.cpp
@@ -0,0 +1,104 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <numeric>
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T> using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template <class K,class V> using omap = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define f first
+#define s second
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define ll long long
+#define pll pair<long long,long long>
+#define vpl vector<pair<long long,long long>>
+#define vll vector<long long>
+#define lb lower_bound
+#define ub upper_bound
+#define Checkbit(a,i) (((a)>>(i))&1)
+#define Setbit(a,i) ((a)^=1LL<<(i))
+
+const int mod = 1e9 + 7;
+
+const int N = 1e6;
+
+
+void solve()
+{
+    int n, m;
+    while (cin >> n >> m) {
+        vi a(m);
+
+        for (int i = 0; i < m; i++) {
+            cin >> a[i];
+        }
+
+        vector<int> dp(n + 5);
+        dp[0] = false;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = false;
+            for (int j : a) {
+                if (i - j >= 0) {
+                    dp[i] |= (!dp[i - j]);
+                }
+            }
+        }
+
+        if (dp[n]) {
+            cout << "Stan wins\n";
+        }
+        else {
+            cout << "Ollie wins\n";
+        }
+    }
+}
+
+signed main()
+{
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    int cs = 1;
+    while (t--) {
+        //cout << "Case " << cs++ << ":" << " ";
+        solve();
+    }
+}
